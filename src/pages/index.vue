0b5570dcdb5fede77ac62d6b3e2e9a47f20f9beb
Dhika Rizky <deka_r@outlook.com>
2018-10-09 22:51:54 +0700

    Changed "Search" text to magnifying glass

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@
   <div class="w-full rounded-sm md:w-1/2 xl:w-1/3 -mt-12 h-24 bg-white shadow md:rounded-lg p-4 table mx-auto">
     <div class="flex mt-3">
       <input placeholder="Try 'Kana Hanazawa'" v-model="query" class="flex-grow h-10 p-2 rounded-lg border-2 border-r-0 border-purple outline-none rounded-tr-none rounded-br-none" type="text">
-      <button @click="find" class="focus:outline-none w-24 h-10 p-2 rounded-tr-lg rounded-br-lg bg-purple text-white">Search</button>
+      <button @click="find" class="focus:outline-none w-12 h-10 p-2 rounded-tr-lg rounded-br-lg bg-purple text-white material-icons">search</button>
     </div>
   </div>
 </div>
