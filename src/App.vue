63eebce7dbf004f8d4414f2ea898e107c8457c1f
Dhika Rizky <deka_r@outlook.com>
2019-03-28 20:31:40 +0700

    Moved footer to main component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
     <navbar/>
-    <router-view class="mt-16 mb-16 md:mb-0"/>
+    <router-view class="mt-16"/>
+    <footer class="mb-16 md:mb-0 p-8">
+      <span class="mt-8 block text-base font-normal text-grey-darker">Voice actor data provided by <a href="https://anilist.co" class="no-underline text-purple-dark" target="_blank">AniList</a> API, Discography data provided by <a href="https://musicbrainz.org" class="no-underline text-purple-dark" target="_blank">MusicBrainz</a></span>
+        <span class="mt-1 block text-base font-normal text-grey-dark">This site is not affiliated or sponsored by AniList and/or MusicBrainz in any way</span>
+        <span class="mt-2 block text-sm font-normal text-grey-dark">
+            Made by 
+            <a href="http://dhikarizky.ga" class="no-underline text-purple-dark" target="_blank">Dhika Rizky</a> 
+            with <a href="https://vuejs.org" class="no-underline text-purple-dark" target="_blank">Vue</a>, 
+            <a href="https://tailwindcss.com" class="no-underline text-purple-dark" target="_blank">Tailwind</a>, 
+            <a href="https://graphql.org" class="no-underline text-purple-dark" target="_blank">GraphQL</a>, 
+            <a href="https://github.com/prisma/graphql-request" class="no-underline text-purple-dark" target="_blank">graphql-request</a>, 
+            <a href="https://github.com/jbraithwaite/nodebrainz" class="no-underline text-purple-dark" target="_blank">nodebrainz</a>,
+             and my spare time
+        </span>
+    </footer>
     <actionbar/>
   </div>
 </template>
--- src/pages/info.vue
+++ src/pages/info.vue
@@ -63,18 +63,6 @@
                 <span class="text-lg mt-1 block font-normal text-grey-dark">If You believe this person exists, try using <strong class="text-grey-darker">different spelling</strong> or <strong class="text-grey-darker">their native name</strong></span>
             </div>
         </div>
-        <span class="mt-8 block text-base font-normal text-grey-darker">Voice actor data provided by <a href="https://anilist.co" class="no-underline text-purple-dark" target="_blank">AniList</a> API, Discography data provided by <a href="https://musicbrainz.org" class="no-underline text-purple-dark" target="_blank">MusicBrainz</a></span>
-        <span class="mt-1 block text-base font-normal text-grey-dark">This site is not affiliated or sponsored by AniList and/or MusicBrainz in any way</span>
-        <span class="mt-2 block text-sm font-normal text-grey-dark">
-            Made by 
-            <a href="http://dhikarizky.ga" class="no-underline text-purple-dark" target="_blank">Dhika Rizky</a> 
-            with <a href="https://vuejs.org" class="no-underline text-purple-dark" target="_blank">Vue</a>, 
-            <a href="https://tailwindcss.com" class="no-underline text-purple-dark" target="_blank">Tailwind</a>, 
-            <a href="https://graphql.org" class="no-underline text-purple-dark" target="_blank">GraphQL</a>, 
-            <a href="https://github.com/prisma/graphql-request" class="no-underline text-purple-dark" target="_blank">graphql-request</a>, 
-            <a href="https://github.com/jbraithwaite/nodebrainz" class="no-underline text-purple-dark" target="_blank">nodebrainz</a>,
-             and my spare time
-        </span>
     </div>
 </template>
 
