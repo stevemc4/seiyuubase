582eb320befa8ec0ea214bf0b13cf8ab529e11cf
Dhika Rizky <deka_r@outlook.com>
2019-03-31 20:16:53 +0700

    Changed how the project builds

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -112,8 +112,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
+        from: path.resolve(__dirname, '../public'),
+        to: config.build.assetsRoot,
         ignore: ['.*']
       }
     ]),
